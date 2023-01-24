7b536b4aed32b089c37aa2c20efd4b8b0dcda06d
SalwaIqlimaKvg <113961202+Salwaiqlimakvg@users.noreply.github.com>
2023-01-24 07:57:21 +0700

    Add files via upload

---
+++ Salwa Iqlima KVG_Tugas 1 (PKTI).py
@@ -0,0 +1,144 @@
+#Menjumlahkan 4 buah angka positif
+nilai1= 2
+nilai2= 3
+nilai3= 4
+nilai4= 5
+hitung=nilai1+nilai2+nilai3+nilai4
+print (hitung)
+
+#Mencari nilai rata-rata UN 
+namalengkap1= "Salwa Iqlima KVG"
+namalengkap2= "Erin Putri"
+namalengkap3= "Syifa Cahaya"
+namalengkap4= "Mona Monika"
+
+B_indonesia1= 80
+B_indonesia2= 85
+B_indonesia3= 70
+B_indonesia4= 90
+
+B_inggris1= 90
+B_inggris2= 80
+B_inggris3= 70
+B_inggris4= 60
+
+matematika1= 90
+matematika2= 95
+matematika3= 80
+matematika4= 90
+
+ipa1= 80
+ipa2= 90
+ipa3= 85
+ipa4= 75
+
+hitung1= (B_indonesia1 + B_inggris1 + matematika1+ipa1)/4
+hitung2= (B_indonesia2 + B_inggris2 + matematika2+ipa2)/4
+hitung3= (B_indonesia3 + B_inggris3 + matematika3+ipa3)/4
+hitung4= (B_indonesia4 + B_inggris4 + matematika4+ipa4)/4
+
+print (namalengkap1,hitung1)
+if(hitung1 > 6):
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
+    
+print (namalengkap2,hitung2)
+if(hitung2 > 6):
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
+    
+print (namalengkap3,hitung3)
+if(hitung3 > 6):
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
+    
+print (namalengkap4,hitung4)
+if(hitung4 > 6):
+    print("Selamat Anda Lulus")
+else:
+    print("Maaf Anda Tidak Lulus")
+    
+
+#Informasi profil saya
+Nim= "2000680"
+Nama= "Salwa Iqlima KVG"
+Alamat= "Kp. Kertamulya, Desa Mekargalih, Kec. Ciranjang, RT/RW 01/01, Kab. Cianjur, Jawa Barat"
+No_HP_dan_whatsApp= "085722894649"
+Email= "salwaiqlimakvg@upi.edu"
+IG= "salwaiqlima.vg"
+Youtube_channel= "Salwa Iqlima K.V.G"
+Cita_cita= "dosen dan pengusaha kuliner"
+Hobi= "membaca"
+
+print (Nim)
+print (Nama)
+print (Alamat)
+print (No_HP_dan_whatsApp)
+print (Email)
+print (IG)
+print (Youtube_channel)
+print (Cita_cita)
+print (Hobi)
+
+
+#Menghitung jarak antara titik A(4,5) ke titik B(10,3)
+#Koordinat titik A
+x1= 4
+y1= 5
+#Koordinat titik B
+x2= 10
+y2= 3
+
+#Mencari X
+x= x2-x1
+print (x)
+#Dipangkatkan
+hitung1= 6**2
+print (hitung1)
+
+#Mencari y
+y= y2-y1
+print (y)
+#Dipangkatkan
+hitung2= (-2)**2
+print (hitung2)
+ 
+#Mencari jarak titik A ke titik B
+#Jarak titik A ke titik B dimisalkan AB
+import math
+AB_pangkat2= hitung1 + hitung2
+print (AB_pangkat2)
+AB= math.sqrt (hitung1 + hitung2)
+print (AB)
+
+
+#Konversi dari meter ke kilo meter
+A= "20 meter"
+hitungA= 20/30
+print (hitungA)
+
+#Konversi dari kaki ke cm
+#rumus
+cm= "berapa kaki/0.032808"
+#contoh soal: konversi dari 2 kaki ke cm berapa?
+cm= 2/0.032808
+print (cm)
+
+#konversi dari inchi ke cm
+#rumus
+cm= "berapa inchi/0.39370"
+#contoh soal: konversi dari 2 inchi ke cm berapa?
+cm= 2/0.39370
+print (cm)
+
+
+
+
+
+
+
+
+
